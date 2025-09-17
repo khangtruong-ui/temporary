4071796b05c35d48ec6ce5c5f642a88a1e4b7096
khangtruong-ui <khang.truong@gmail.com>
2025-09-17 17:32:36 +0700

    Update and rename notebook_converted.py to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-"""Converted from Re_remote_Object_detection.ipynb - main logic in `run_inference(image_bytes)`"""
-import io, os, sys
-import PIL  # auto-added
+mkdir -p /content/Images
+mkdir -p /content/result
+git clone https://huggingface.co/KhangTruong/vit-rvsa
+cd vit-rvsa
 
+pip install -q numpy==1.26.4
 # --- cell 1 ---
 pip install -q torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cpu
 pip install -q mmcv==2.0.1 -f https://download.openmmlab.com/mmcv/dist/cpu/torch2.0/index.html
@@ -21,9 +23,3 @@ mv 02_NWPU_RESISC45 data
 mv 02_NWPU_caption data
 python install.py --root .
 cd mmrotate
-
-# --- cell 5 ---
-from PIL import Image
-for i in range(1, 11):
-    directory = f'/content/Images/{i:02d}.png'
-    # directory = '../data/airplane/airplane_001.jpg'
