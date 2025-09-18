e67eca6f00a6967074ae5dad4850ad7005148ec8
khangtruong-ui <khang.truong@gmail.com>
2025-09-18 10:45:03 +0700

    Update extra_code.py

--- extra_code.py
+++ extra_code.py
@@ -2,5 +2,5 @@ with open('vit-rvsa/install.py') as f:
     content = f.read()
 
 with open('vit-rvsa/install.py', 'w') as f:
-    new_content = content.replace('use_abs_pos_emb=True', 'use_abs_pos_emb=False')
+    # new_content = content.replace('use_abs_pos_emb=True', 'use_abs_pos_emb=False')
     f.write(new_content)
